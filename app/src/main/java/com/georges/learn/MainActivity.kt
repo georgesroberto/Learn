3c692b2e8871b5ab75ed70acba0b14e8c0b9b1ea
georgesroberto <georgesroberto21@gmail.com>
2024-03-16 12:38:51 +0300

    Implemented DiceRoller Logic

--- app/src/main/java/com/georges/learn/MainActivity.kt
+++ app/src/main/java/com/georges/learn/MainActivity.kt
@@ -3,13 +3,23 @@ package com.georges.learn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.georges.learn.ui.theme.LearnTheme
 
@@ -44,5 +54,26 @@ fun DiceRollerApp(){
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
 
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
+        Button(onClick = {
+            result = (1..6).random()
+
+        } ) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
