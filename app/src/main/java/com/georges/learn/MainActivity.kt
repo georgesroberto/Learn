6c51d4c6e7d2df482f6cffb7af07114c58529606
georgesroberto <georgesroberto21@gmail.com>
2024-03-19 09:55:34 +0300

    Instantiated App Structure

--- app/src/main/java/com/georges/learn/MainActivity.kt
+++ app/src/main/java/com/georges/learn/MainActivity.kt
@@ -3,16 +3,21 @@ package com.georges.learn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.georges.learn.ui.theme.LearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    GreetingMessage(name = "Georges")
+                    SpaceApp()
                 }
             }
         }
@@ -35,11 +40,42 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreetingMessage(name: String) {
-    Text(
-        text = "Welcome back, $name!",
-        style = MaterialTheme.typography.headlineLarge,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.fillMaxWidth()
-    )
+fun SpaceApp() {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+    ){
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(12.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Art Title")
+            Text(text = "Artist")
+        }
+
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(12.dp)
+                .align(Alignment.CenterHorizontally)
+        ){
+            Button(onClick = { /*TODO*/ }) {
+
+            }
+            Button(onClick = { /*TODO*/ }) {
+
+            }
+        }
+    }
 }
