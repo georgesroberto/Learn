04d6f1d43f90d842bb60e1456df554b67e28126a
georgesroberto <georgesroberto21@gmail.com>
2024-03-20 11:01:18 +0300

    String values

--- app/src/main/java/com/georges/learn/MainActivity.kt
+++ app/src/main/java/com/georges/learn/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,7 +18,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.georges.learn.ui.theme.LearnTheme
 
@@ -46,22 +52,26 @@ fun SpaceApp() {
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
+            .fillMaxHeight()
     ){
         Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
+            painter = painterResource(R.drawable.image1),
             contentDescription = null,
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(0.8f)
+                .align(Alignment.CenterHorizontally)
+                .padding(12.dp)
+                .border(width = 4.dp, color = Color.Blue, shape = RectangleShape)
         )
 
         Column(
             verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .padding(12.dp)
+                .padding(2.dp)
                 .align(Alignment.CenterHorizontally)
         ) {
-            Text(text = "Art Title")
-            Text(text = "Artist")
+            Text(text = stringResource(R.string.imag1_title))
+            Text(text = stringResource(R.string.imag1_author))
         }
 
         Row (
