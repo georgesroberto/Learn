fa335764535966398e6dc7d40198c52856f6cc32
georgesroberto <georgesroberto21@gmail.com>
2024-03-27 09:35:24 +0300

    Initialized

--- app/src/main/java/com/georges/learn/model/Dessert.kt
+++
@@ -1,6 +0,0 @@
-package com.georges.learn.model
-
-/**
- * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
- */
-data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
---
+++ app/src/main/java/com/georges/learn/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.georges.learn.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(10.dp),
+    large = RoundedCornerShape(16.dp)
+)
