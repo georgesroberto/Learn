1459bc8e1164cb3b40f622cc829339d12c1c25bb
georgesroberto <georgesroberto21@gmail.com>
2024-04-02 07:21:50 +0300

    Implement Functionality + Internet permission

--- app/src/main/java/com/georges/learn/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/georges/learn/ui/screens/MarsViewModel.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.georges.learn.network.MarsApi
+import kotlinx.coroutines.launch
 
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
@@ -22,6 +25,9 @@ class MarsViewModel : ViewModel() {
      * [MarsPhoto] [List] [MutableList].
      */
     fun getMarsPhotos() {
-        marsUiState = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            val listResult = MarsApi.retrofitService.getPhotos()
+            marsUiState = listResult
+        }
     }
 }
